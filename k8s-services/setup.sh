ccb52aef22d7bd2e9713c532236758e13100b31d
govindael <govinda@everledger.io>
2019-02-16 02:16:35 +1100

    helm charts working too for api

--- k8s-services/setup.sh
+++ k8s-services/setup.sh
@@ -2,4 +2,9 @@
 kubectl create -f rbac-config.yaml
 helm init --service-account tiller
 
+
 helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true
+
+helm install --name demo stable/postgresql --namespace db --set postgresqlPassword=postgres,postgresqlDatabase=postgres
+
+kubectl apply -f adminer.yml -n db
